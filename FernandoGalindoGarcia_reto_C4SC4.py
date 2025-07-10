a9b7ca0274f49e4a38671c22ea4d34903c7b2886
Fernando Galindo Garcia <fgalindo@outlook.com>
2025-07-10 17:34:03 -0600

    Commit inicial: Streamlit app para app de empleados - Reto

---
+++ FernandoGalindoGarcia_reto_C4SC4.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# 1. Títulos y descripción del dashboard
+st.title('Reto Fernando Galindo C4SC4')
+st.header('Streamlit Dashboard')
+st.subheader('Analisis de la base de datos de empleados de la empresa Socialize your Knowledge')
+
+# 2. Logo de la empresa
+image_url = "img\logo_.jpeg"
+st.image(image_url, caption='Social Knowledge', use_container_width=True) 
+
